728d1cffa4553009a272ced4ec93652df6aa8402
rubeeengm <rubeeengm@gmail.com>
2019-07-11 00:05:51 -0500

    implemented the hoverEffect successfully

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+	parent: document.querySelector('.distortion'),
+	intensity: 0.2,
+	image1: 'assets/img/bw1.jpg',
+	image2: 'assets/img/bw2.jpg',
+	displacementImage: 'assets/img/heightMap.png'
+});
